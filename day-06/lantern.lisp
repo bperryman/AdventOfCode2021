2b63e0e50627f6fd376fd1c61651dee688223570
Barry Perryman <barry.perryman@gmail.com>
2021-12-07 13:40:08 +0000

    Day 6 - Lantern fish
    Updated with a much better, not brute force, algorithm.

--- day-06/lantern.lisp
+++ day-06/lantern.lisp
@@ -1,32 +1,33 @@
 ;;;; lantern fish problem
+(defun make-population ()
+  (make-array 9 :initial-element 0))
+
+(defun add-fish-to-population (population day-cycle &optional (quantity 1))
+  (incf (aref population day-cycle) quantity))
+
+(defun population-size (population)
+  (loop for fish-per-cycle across population
+        sum fish-per-cycle))
+
 (defun process-lantern-file (file)
   (with-open-file (stream file)
     (let ((line (substitute #\space #\, (read-line stream)))
-          (fish (make-array 100
-                            :adjustable t
-                            :fill-pointer 0
-                            :element-type 'fixnum
-                            :initial-element 0)))
+          (population (make-population)))
       (with-open-stream (ss (make-string-input-stream line))
         (do ((entry (read ss nil) (read ss nil)))
-            ((null entry) fish)
-          (vector-push-extend entry fish))))))
+            ((null entry) population)
+          (add-fish-to-population population entry))))))
 
-(defun vector-push-all (src dest)
-  (loop for entry across src
-        do (vector-push-extend entry dest)))
+(defun rotate-vector (vector)
+  (let ((zero-value (aref vector 0)))
+    (loop for i from 0 to (- (length vector) 2)
+          do
+             (setf (aref vector i) (aref vector (1+ i))))
+    (setf (aref vector (1- (length vector))) zero-value)))
 
 (defun simulation (input-file days)
   (let ((population (process-lantern-file input-file)))
-    (dotimes (day days (length population))
-      (let ((size (length population)))
-        (loop for i from 0 below size
-              with children = (make-array (floor size 7)
-                                          :element-type 'fixnum
-                                          :adjustable t
-                                          :fill-pointer 0)
-              when (< (decf (aref population i)) 0)
-                do
-                   (setf (aref population i) 6)
-                   (vector-push-extend 8 children)
-              finally (vector-push-all children population))))))
+    (dotimes (day days (population-size population))
+      (let ((day-0 (aref population 0)))
+        (rotate-vector population)
+        (add-fish-to-population population 6 day-0)))))
